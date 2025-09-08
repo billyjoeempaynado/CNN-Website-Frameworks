92a63dc8022df2513c0cb08966819ba62d48a750
Bijanggg <44258483+billyjoeempaynado@users.noreply.github.com>
2025-09-08 12:01:30 +0800

    upload

--- app/about/page.js
+++ app/about/page.js
@@ -7,7 +7,7 @@ import TraderSection from "../components/TraderSection";
 
 export default function AboutPage() {
   return (
-    <main className="bg-gray-100 pt-20">
+    <main className="bg-gray-100 dark:bg-gray-900 pt-20">
       {/* About Us Header */}
       <section className="section-about bg-gray-100 dark:bg-gray-900 py-16 text-center">
         <div className="container mx-auto px-6">
